0e55048db1a041a02cf6790323f3efe5a7987b79
ccastri <68607137+ccastri@users.noreply.github.com>
2023-09-16 12:23:38 -0500

    Image index fixings

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -84,7 +84,7 @@ function FeaturedCollection({
   return (
 <>
       {/* <h3 className="text-center font-bold leading-tight tracking-widest text-[#BB6A72] py-2 text-3xl">¡Enterate de las nuevas tendencias en Alma!</h3> */}
-    <div className='w-full flex  items-center md:justify-between h-screen md:space-x-8 justify-center  md:px-20  md:h-full'>
+    <div className='w-full flex xs:flex-col items-center md:justify-between h-screen md:space-x-8 justify-center  md:px-20  md:h-full'>
       <div className='md:h-[50%] md:w-[60%]  '>
 
     <img src="/european-like-street.jpeg" className=' md:w-full md:px-14 object-cover h-screen' alt="" />
